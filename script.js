d1d8603fda6225a62b3a03a0985aa5b4be665662
enireves <enirevescode@gmail.com>
2023-07-10 11:05:55 +0200

    fction connection

---
+++ script.js
@@ -0,0 +1,16 @@
+// Choix entre inscription ou identification du compte
+
+// Connection
+
+// 
+//let btnConnection = document.getElementById("btnConnection");
+
+// btnConnection.addEventListener("click", () => {
+//    console.log("hey t es connecté")
+// })
+
+let btnConnection = document.getElementById("btnConnection")
+
+ btnConnection.addEventListener("click", function () {
+       console.log("hey t' es connecté")
+    })

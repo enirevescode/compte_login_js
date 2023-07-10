892ff87d73820c12a94d994d5fdc92e08bf4d87b
enireves <enirevescode@gmail.com>
2023-07-10 11:18:48 +0200

    modif connection

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Choix entre inscription ou identification du compte
+
 
 // Connection
 
-// 
-//let btnConnection = document.getElementById("btnConnection");
-
-// btnConnection.addEventListener("click", () => {
-//    console.log("hey t es connecté")
-// })
 
 let btnConnection = document.getElementById("btnConnection")
 
  btnConnection.addEventListener("click", function () {
        console.log("hey t' es connecté")
     })
+
+
+// Choix entre inscription ou identification du compte

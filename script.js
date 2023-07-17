975a731330443a49d407234e342530df5fff2341
enirevescode <enirevescode@gmail.com>
2023-07-17 11:34:13 +0200

    changements

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ let btnConnection = document.getElementById("btnConnection")
 
 
 // Choix entre inscription ou identification du compte
+// function addEventListenerNoCompte() {
+//    let noCompte = document.getElementById("noCompte")
+//    noCompte.addEventListener("click", (noCompte) => {
+//       console.log("s'inscrire")
+//    } )
+// }
+
+//Fction pour émuler le popup signup
+function afficherPopup() {
+   let btnPasCpte = document.getElementById("btnPasCpte")
+   // La popup est masquée par défaut (display:none), ajouter la classe "active"
+   // va changer son display et la rendre visible. 
+   btnPasCpte.classList.add("active")
+}
+function addEventListenerSignUp() {
+   let signUp = document.getElementById("signup")
+   signUp.addEventListener("click", () => {
+      afficherPopup()
+   }) }
